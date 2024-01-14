1352c6d382861d4ac50bd7df77ac7bf99483dbaa
Aditya Bhaumik <92214013+aditya-bhaumik@users.noreply.github.com>
2024-01-15 00:34:17 +0530

    Create solution.java

---
+++ Count Digits/solution.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.evenlyDivides(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+// Simple Java Solution 
+
+class Solution{
+    static int evenlyDivides(int N){
+        // code here
+        int count = 0, rem;
+
+        int temp = N;
+        while(temp > 0){
+            rem = temp % 10;
+            if(rem != 0 && N % rem == 0){
+                count++;
+            }
+            temp = temp / 10;
+        }
+        return count;
+    }
+}
